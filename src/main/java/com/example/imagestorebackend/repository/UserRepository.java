ff9c5af305814685775e08a9ce0e5144e8d803a6
DanielKoltze <dani64h6@stud.kea.dk>
2022-11-14 14:00:48 +0100

    user

--- src/main/java/com/example/imagestorebackend/repository/UserRepository.java
+++ src/main/java/com/example/imagestorebackend/repository/UserRepository.java
@@ -1,7 +1,7 @@
 package com.example.imagestorebackend.repository;
 
-import com.example.imagestorebackend.model.User;
+import com.example.imagestorebackend.model.UserDude;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends CrudRepository<UserDude,Long> {
 }
